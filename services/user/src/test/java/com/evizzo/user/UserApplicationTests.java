b28a117c13b3b86b5142d258ba4deebad786c5a1
Evizzo <stefanmatic941@gmail.com>
2024-09-11 10:42:26 +0200

    Dockerized the application.

---
+++ services/user/src/test/java/com/evizzo/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evizzo.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
